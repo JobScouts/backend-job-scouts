b44d36600e813c2ecab43a8bdc2a496355c76927
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-24 13:08:26 +0000

    samara

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const router = Router();
 const {SECRET_API}=require('../config')
 /////// Route to get all jobs from 3-party-API
-router.get("/jobSearch" , async (req ,res , next) => {
+router.get("/" , async (req ,res , next) => {
     try {
         let jobTitle  = req.query.jobTitle;
         let country  = req.query.country;
