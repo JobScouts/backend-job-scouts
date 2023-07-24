bd7ed9217bd6ef6154e3c96e83584cbd1c4e1513
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-24 12:56:01 +0000

    solve

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const router = Router();
 const {SECRET_API}=require('../config')
 /////// Route to get all jobs from 3-party-API
-router.get("/" , async (req ,res , next) => {
+router.get("/jobSearch" , async (req ,res , next) => {
     try {
         let jobTitle  = req.query.jobTitle;
         let country  = req.query.country;
@@ -16,7 +16,7 @@ router.get("/" , async (req ,res , next) => {
     num_pages: '1'
   },
   headers: {
-    'X-RapidAPI-Key': SECRET_API,
+    'X-RapidAPI-Key': `${SECRET_API}`,
     'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
   }
 };
@@ -27,20 +27,22 @@ router.get("/" , async (req ,res , next) => {
             "employer_name" : result.employer_name,
             "employer_logo": result.employer_logo?result.employer_logo:"",
             "employer_website": result.employer_website?result.employer_website :"" ,            
-            "job_employment_type": result.job_employment_type, // FullTime / partTime
+            "job_employment_type": result.job_employment_type,
+            // "job_publisher":result.job_publisher,
             "job_title": result.job_title,
             "job_description": result.job_description,
             "job_is_remote" : result.job_is_remote,
             "job_city": result.job_city,
             "job_country" : result.job_country,  
-            // "job_google_link": result.job_google_link,
-            "job_apply_link": result.job_apply_link?result.job_apply_link:"",
+            "job_google_link": result.job_google_link,
+            "job_apply_link": result.job_apply_link,
             "job_highlights": result.job_highlights && result.job_highlights.Qualifications ? result.job_highlights.Qualifications.join(' * ') : "",          
-            // "job_min_salary": result.job_min_salary,
-            // "job_max_salary": result.job_max_salary,
+            "job_min_salary": result.job_min_salary,
+            "job_max_salary": result.job_max_salary,
 
             
         }));
+// hell
         res.send(jobs);
     } catch (error) {
         console.error(error);
