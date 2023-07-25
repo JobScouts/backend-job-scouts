97afd037ebc86181fa7c89b9568f1a8c2b03a191
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-25 19:10:44 +0000

    ss

--- server.js
+++ server.js
@@ -36,6 +36,34 @@ app.use(authRouter);
 
 /////////// Handle Error Routes
 
+app.patch('/update', (req, res) => {
+  let { about } = req.body;
+  let data = JSON.stringify({
+    app_metadata: { about },
+  });
+
+  let config = {
+    method: 'patch',
+    url: 'https://login.auth0.com/api/v2/users/google-oauth2%7C100737014092563339568',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      res.status(200).json({ message: 'Successfully updated about.' });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Error updating about.' });
+    });
+});
+
+
 app.use(handleErrorServer);
 app.use(handleErrorNotFound);
 
